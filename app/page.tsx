4fab32ea04506a2da79c7157a677ba91a76839e7
Carlos Rojas <carloshenri2001@gmail.com>
2025-12-22 21:10:50 -0300

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,92 @@
-import { Button } from "@/components/ui/button"
+import { ChartOverview } from '@/components/chart';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { BadgeDollarSign, DollarSign, Percent, Users } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="px-6">
-      <h2 className="font-bold">Teste</h2>
-      <Button>Cadastrar</Button>
-    </div>
+    <main className="p-4" cz-shortcut-listen="true">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card>
+          <CardHeader>
+            <div className='flex items-center justify-center'>
+              <CardTitle className='text-lg sm:text-xl text-gray-800 select-none'>
+                Total Vendas
+              </CardTitle>
+              <DollarSign className='ml-auto w-4 h-4'/>
+            </div>
+
+            <CardDescription>
+              Total de vendas em 90 dias
+            </CardDescription>
+
+          </CardHeader>
+          <CardContent>
+            <p className='text-base sm:text-lg font-bold'>R$ 40.000</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className='flex items-center justify-center'>
+              <CardTitle className='text-lg sm:text-xl text-gray-800 select-none'>
+                Novos Clientes
+              </CardTitle>
+              <Users className='ml-auto w-4 h-4'/>
+            </div>
+
+            <CardDescription>
+              Novos clientes em 30 dias
+            </CardDescription>
+
+          </CardHeader>
+          <CardContent>
+            <p className='text-base sm:text-lg font-bold'>234</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className='flex items-center justify-center'>
+              <CardTitle className='text-lg sm:text-xl text-gray-800 select-none'>
+                Pedidos Hoje
+              </CardTitle>
+              <Percent className='ml-auto w-4 h-4'/>
+            </div>
+
+            <CardDescription>
+              Total de pedidos hoje
+            </CardDescription>
+
+          </CardHeader>
+          <CardContent>
+            <p className='text-base sm:text-lg font-bold'>65</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className='flex items-center justify-center'>
+              <CardTitle className='text-lg sm:text-xl text-gray-800 select-none'>
+                Total pedidos
+              </CardTitle>
+              <BadgeDollarSign className='ml-auto w-4 h-4'/>
+            </div>
+
+            <CardDescription>
+              Total de pedidos em 30 dias
+            </CardDescription>
+
+          </CardHeader>
+          <CardContent>
+            <p className='text-base sm:text-lg font-bold'>1300</p>
+          </CardContent>
+        </Card>
+
+      </section>
+
+      <section className='mt-4 flex flex-col-3 md-flex-row gap-4'>
+        <ChartOverview></ChartOverview>
+      </section>
+    </main>
   );
 }
